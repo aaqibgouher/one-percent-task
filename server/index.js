6e61481c5652c4e26ac124b652cfa2d79fe715ff
aaqibgouher <aaqibgouher@gmail.com>
2024-01-23 19:49:10 +0530

    changed client url in server

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3001;
 // Specify allowed origins based on the environment
 const allowedOrigins =
   process.env.NODE_ENV === "production"
-    ? ["https://your-prod-domain.com"]
+    ? ["https://one-percent-task-client.vercel.app"]
     : [`http://localhost:5173`];
 
 app.use(
